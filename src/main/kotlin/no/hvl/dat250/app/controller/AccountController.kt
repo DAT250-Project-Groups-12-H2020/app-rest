62a8e522356ffea1e3a34e64bc0daa2ef35d1804
Elg <klink100@gmail.com>
2020-10-05 14:09:16 +0200

    Add deletion and updating of accounts

--- src/main/kotlin/no/hvl/dat250/app/controller/AccountController.kt
+++ src/main/kotlin/no/hvl/dat250/app/controller/AccountController.kt
@@ -1,16 +1,22 @@
 package no.hvl.dat250.app.controller
 
 import no.hvl.dat250.app.API_VERSION_1
+import no.hvl.dat250.app.dto.AccountRequest
 import no.hvl.dat250.app.dto.AccountResponse
 import no.hvl.dat250.app.dto.PublicAccountResponse
 import no.hvl.dat250.app.dto.toPublicResponse
 import no.hvl.dat250.app.dto.toResponse
 import no.hvl.dat250.app.service.AccountService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 /**
  * @author Elg
@@ -27,8 +33,33 @@ class AccountController {
     return accountService.getCurrentAccount().toResponse()
   }
 
+  @PutMapping("/me")
+  fun updateMe(@Valid @RequestBody accountRequest: AccountRequest): AccountResponse {
+    val account = accountService.getCurrentAccount()
+    return accountService.updateAccount(account.id, accountRequest).toResponse()
+  }
+
+  @DeleteMapping("/me")
+  fun deleteMe(): AccountResponse {
+    val account = accountService.getCurrentAccount()
+    accountService.deleteAccount(account.id)
+    return account.toResponse()
+  }
+
   @GetMapping
   fun publicAccountInformation(@RequestParam("id") id: String): PublicAccountResponse {
     return accountService.getAccountByUid(id).toPublicResponse()
+  }
+
+  @PutMapping("/{id}")
+  fun updateAccount(@PathVariable id: String, @Valid @RequestBody accountRequest: AccountRequest): AccountResponse {
+    return accountService.updateAccount(id, accountRequest).toResponse()
+  }
+
+  @DeleteMapping("/{id}")
+  fun deleteAccount(@PathVariable id: String): AccountResponse {
+    val account = accountService.getCurrentAccount()
+    accountService.deleteAccount(id)
+    return account.toResponse()
   }
 }
