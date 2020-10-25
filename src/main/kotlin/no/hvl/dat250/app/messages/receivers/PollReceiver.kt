a3d79d9452d88cc8db517984bd6d2e60735a7af1
Elg <klink100@gmail.com>
2020-10-25 16:27:05 +0100

    Publish dweet when a poll opens and closes

--- src/main/kotlin/no/hvl/dat250/app/messages/receivers/PollReceiver.kt
+++ src/main/kotlin/no/hvl/dat250/app/messages/receivers/PollReceiver.kt
@@ -1,15 +1,33 @@
 package no.hvl.dat250.app.messages.receivers
 
+import no.hvl.dat250.app.messages.Dweet
+import no.hvl.dat250.app.model.Poll
+import no.hvl.dat250.app.repository.PollRepository
+import org.hibernate.Hibernate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @Component
-class PollReceiver {
+class PollReceiver(@Autowired val dweet: Dweet, @Autowired val pollRepository: PollRepository) {
+
+  private fun processPoll(pollId: Long): Poll? {
+    val poll = pollRepository.findByIdOrNull(pollId) ?: return null
+    Hibernate.initialize(poll.votes)
+    return poll
+  }
 
   fun handleOpened(pollId: Long) {
+    val poll = processPoll(pollId) ?: return
+    dweet.sendOpenedDweet(poll)
     println("Poll opened: $pollId")
   }
 
   fun handleClosed(pollId: Long) {
+    val poll = processPoll(pollId) ?: return
+    dweet.sendClosedDweet(poll)
     println("Poll closed: $pollId")
   }
 }
