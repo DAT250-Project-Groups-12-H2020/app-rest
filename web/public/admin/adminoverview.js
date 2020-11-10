e5d062442361cc1ec8b0a7c5ad2346199f8907a8
Emily Nguyen <emily.nguyen@student.uib.no>
2020-11-10 10:02:39 +0100

    added an s in pollsoverview

--- web/public/admin/adminoverview.js
+++ web/public/admin/adminoverview.js
@@ -27,7 +27,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 function poll_overview(){
-    location.href = "../poll/polloverview.html";
+    location.href = "../poll/pollsoverview.html";
 }
 
 function user_overview(){
