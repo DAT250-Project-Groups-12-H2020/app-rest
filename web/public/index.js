b2f3736aab53fc246cd1b3baafe8d990b1075e33
Emily Nguyen <emily.nguyen@student.uib.no>
2020-11-15 23:55:44 +0100

    removed firebase from index

--- web/public/index.js
+++ web/public/index.js
@@ -1,32 +1,3 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-
-        //document.getElementById("user_div").style.display = "block";
-        //document.getElementById("login_div").style.display = "block";
-        //document.getElementById("poll_div").style.display = "block";
-
-        var user = firebase.auth().currentUser;
-
-        if(user != null){
-
-            //var email_id = user.email;
-            //document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-
-        }
-
-    } else {
-        // No user is signed in.
-
-        //document.getElementById("user_div").style.display = "block";
-        //document.getElementById("login_div").style.display = "block";
-
-    }
-    //document.getElementById("poll_div").style.display = "block";
-
-});
-
-
 function email_password(){
     location.href = "login/email-password.html";
 }
